855ed4b73d0ab4f081b6ce1f85738dfaef96ef0d
GNorth <grief.north@gmail.com>
2024-12-23 00:29:46 +0300

    add gapps support

--- lineage_tb128fu.mk
+++ lineage_tb128fu.mk
@@ -11,6 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 WITH_GMS := true
 TARGET_DISABLE_EPPE := true
 
+# Gapps
+ifeq ($(WITH_GMS), false)
+$(call inherit-product,vendor/gapps/arm64/arm64-vendor.mk)
+endif
+
 # Inherit some common Evolution-X stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
